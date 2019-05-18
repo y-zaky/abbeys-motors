9e86046c8a632943a47cf89237a7cdb3b2ff6710
Yahia <zaky20@hotmail.com>
2019-05-18 11:57:39 +0100

    logo complete

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          
+          <div className="texts"></div>
+            <div className="App-header-text">
+            <h2>Welcome to Abbey's Motors</h2>
+            <h2>We've got dodgy vehicles to suit your needs!</h2>
+            </div>
+          </div>
+          
+        </div>
+        
+     
+    );
+  }
+}
+
+export default App;
