b10f437df81f5c4754b7ae05a2d41bfaa20399dc
Yahia <zaky20@hotmail.com>
2019-10-05 13:00:05 +0100

    code refactor with js

--- src/carGrid.js
+++ src/carGrid.js
@@ -5,57 +5,26 @@ import car3 from '../public/car3.jpg'
 // toDo  create card grid components, sizes and css of card grid components, make some kind of api call somewhere. 
 class CarGrid extends Component {
   render() {
+
+    const gridRows = Array(4).fill(1).map( (row,index) => 
+          <div className='row' key={index+'row'}>
+            <div className='car-card' key={index}>
+              <img src={car1} className="car" alt="logo" />
+            </div>
+            <div className='car-card'>
+              <img src={car2} className="car" alt="logo" />
+            </div>
+              <div className='car-card'>
+              <img src={car3} className="car" alt="logo" />
+            </div>
+          </div> 
+    )
+
     return (
       <div className='car-grid'>
 
-        <div className='row'>
-          <div className='car-card'>
-            <img src={car1} className="car" alt="logo" />
-          </div>
-          <div className='car-card'>
-            <img src={car2} className="car" alt="logo" />
-          </div>
-            <div className='car-card'>
-            <img src={car3} className="car" alt="logo" />
-          </div>
-        </div>
-
-
-        <div className='row'>
-          <div className='car-card'>
-            <img src={car1} className="car" alt="logo" />
-          </div>
-          <div className='car-card'>
-            <img src={car2} className="car" alt="logo" />
-          </div>
-            <div className='car-card'>
-            <img src={car3} className="car" alt="logo" />
-          </div>
-        </div>
-
-        <div className='row'>
-          <div className='car-card'>
-            <img src={car1} className="car" alt="logo" />
-          </div>
-          <div className='car-card'>
-            <img src={car2} className="car" alt="logo" />
-          </div>
-            <div className='car-card'>
-            <img src={car3} className="car" alt="logo" />
-          </div>
-        </div>
-
-        <div className='row'>
-          <div className='car-card'>
-            <img src={car1} className="car" alt="logo" />
-          </div>
-          <div className='car-card'>
-            <img src={car2} className="car" alt="logo" />
-          </div>
-            <div className='car-card'>
-            <img src={car3} className="car" alt="logo" />
-          </div>
-        </div>
+        {gridRows}
+    
       </div>
     )
 
