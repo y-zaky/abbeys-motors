2c5e3d27a2bbf6e6edf7f23ce9924d911d6921f9
Yahia <zaky20@hotmail.com>
2019-10-05 13:53:28 +0100

    menu image added

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import CarGrid from './carGrid'
+import Navigation from './navigation'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+          
+              <Navigation/>
           <div className="texts">
             <div className="App-header-text">
               <h2>Welcome to Abbey's Motors</h2>
---
+++ src/navigation.js
@@ -0,0 +1,21 @@
+import React, {Component} from 'react';
+import navigationImage from './navigationImage.png'
+
+class Navigation extends Component {
+  render() {
+    
+
+   
+    return (
+      
+
+      <div className='navigationImage-container' >
+
+        <img className='navigationImage' src={navigationImage}/>>
+      </div>
+    )
+
+  }
+}
+
+export default Navigation;
