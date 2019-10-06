666315ca93d44c5170b3acf4a631c29e40abfbc8
Yahia <zaky20@hotmail.com>
2019-10-06 23:29:24 +0100

    nav burger menu css

--- src/navigation.js
+++ src/navigation.js
@@ -2,19 +2,30 @@ import React, {Component} from 'react';
 import navigationImage from './navigationImage.png'
 
 class Navigation extends Component {
+  constructor(){
+    super()
+    this.state = {
+      isClicked: false
+    }
+  }
   render() {
-    
 
-   
-    return (
-      
+    const { isClicked } = this.state
 
-      <div className='navigationImage-container' >
-
-        <img className='navigationImage' src={navigationImage}/>>
+   return isClicked
+    ?
+      <div className='navigation--open' >
+        <li className='navigation--button-close' onClick={(click) =>  this.setState({isClicked:!isClicked})} >&times;</li>
+        <React.Fragment>
+          <li>Home</li>
+          <li>Contact us</li>
+          <li>About us</li>
+        </React.Fragment>
+      </div>  
+    :
+      <div className='navigationImage-container'>
+        <img className='navigationImage' alt='menu icon' src={navigationImage} onClick={() => this.setState({isClicked:!isClicked})}/>
       </div>
-    )
-
   }
 }
 
